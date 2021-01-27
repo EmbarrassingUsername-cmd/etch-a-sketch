e1e50d2998650d25108f33b0e3357812b9c21a48
Jamie Bukraba <Jamie.Bukraba@googlemail.com>
2021-01-27 23:57:59 +0000

    added reset button and ability to specify grid size needs adjustments to CSS to form grid with other numbers

--- etchasketch.js
+++ etchasketch.js
@@ -1,18 +1,9 @@
 const etchASketchContainer=document.querySelector(".etch-a-sketch-holder")
-let numberofPixels=10000
-
-for(let i=0; i<numberofPixels;i++){
-const squareDivPixels=document.createElement("div")
-squareDivPixels.classList="pixels"
-etchASketchContainer.appendChild(squareDivPixels)
-}
-
-
-const pixelsInDOM=Array.from(document.querySelector(".etch-a-sketch-holder").childNodes)
-console.log(pixelsInDOM)
-pixelsInDOM.forEach(item=>item.addEventListener("mouseover", shadeUp))
-pixelsInDOM.forEach(item=>item.addEventListener("mouseover", shadeUp))
-
+let numberofPixels=4096
+let pixelsInDOM =""
+const resetButton=document.querySelector("#reset")
+resetButton.addEventListener("click", resetPixels)
+createGrid(numberofPixels)
 
 function shadeUp(){
     let classOfDom=`${this.classList[1]}`
@@ -28,5 +19,26 @@ function colourToBlack(){
     this.classList.add("black-pixel")
 }
 function resetPixels(){
-    this.classList="pixels"
+    while(etchASketchContainer.firstChild)
+    etchASketchContainer.removeChild(etchASketchContainer.lastChild)
+    resizeGrid()
+}
+
+function resizeGrid(){
+    let newSize= prompt("Please specify the width of the new grid")
+        if(newSize<4||newSize>100||isNaN(newSize)){
+            while(newSize<4||newSize>100||isNaN(newSize)){
+            newSize=prompt("Only values between 4 and 100 are accepted please specify a value in this range")
+            }
+        }
+    createGrid(newSize*newSize)
+}
+function createGrid(numberofPixels){
+    for(let i=0; i<numberofPixels;i++){
+    const squareDivPixels=document.createElement("div")
+    squareDivPixels.classList="pixels"
+    etchASketchContainer.appendChild(squareDivPixels)
+}
+pixelsInDOM=Array.from(document.querySelector(".etch-a-sketch-holder").childNodes)
+pixelsInDOM.forEach(item=>item.addEventListener("mouseover", shadeUp))
 }
